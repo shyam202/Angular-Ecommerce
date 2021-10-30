273fae962d9b5c4204ec1e8e657de02aeb0c1f89
shyam202 <lakhanishyam11@gmail.com>
2021-10-30 14:56:38 +0200

    Home & Products commited

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './component/home/home.component';
+import { ProductsComponent } from './component/products/products.component';
+import { CartComponent } from './component/cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  providers: [NgbCarouselConfig]
+})
+export class HomeComponent implements OnInit {
+
+  images = ['../../../assets/images/slider/slider-img1.jpg','../../../assets/images/slider/slider-img2.jpg','../../../assets/images/slider/slider-img3.jpg'];
+  router: any;
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
